411e447acac8b145503fb4fda47c273830fa4a5d
Joeri <joerikock@gmail.com>
2019-07-03 16:14:45 +0200

    Working traffic generator

---
+++ trafficGenerator.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import json
+import random
+import time
+import socket
+import struct
+import subprocess
+import shutil
+
+
+def uint32_to_ip(ipn):
+	t = struct.pack('I', ipn)
+	return socket.inet_ntoa(t)
+
+
+def ip_to_uint32(ip):
+	t = socket.inet_aton(ip)
+	return struct.unpack('I', t)[0]
+
+
+def generateTxts(fp, overlap):
+	ip_list = []
+	for item in fp['src_ips']:
+		ip_list.append(ip_to_uint32(item['ip']))
+	resultList = random.sample(ip_list, round((len(ip_list)/100*overlap)))
+	filename = str(overlap) + '.txt'
+	with open(os.path.join('./',filename), 'w') as f:
+		for ip in resultList:
+			f.write('%s\n' % ip)
+
+
+if __name__ == '__main__':
+	start = time.time()
+	overlap_set = [10,20,30]
+
+	# Load fingerprint from argv
+	if len(sys.argv) == 2:
+		f = open(sys.argv[1], 'r')
+		fingerprint = json.loads(f.read())
+	else:
+		raise ValueError('No fingerprint file supplied.')
+	
+	# This line extracts the ID from the fingerprint file (might use later)
+	filename = os.path.splitext(os.path.basename(sys.argv[1]))[0] 
+	
+	# Delete all files in /traffic for a fresh start
+	#shutil.rmtree('./traffic')
+	#os.makedirs('traffic')
+
+	for overlap in overlap_set:
+		generateTxts(fingerprint, overlap)
+	subprocess.call(["make","-f","makefile","all"])
+	for overlap in overlap_set:
+		print("Generating PCAP for",overlap,"percent overlap")
+		subprocess.call(["./randomize", str(overlap)])
+
+	# Clean up
+	subprocess.call(["make","-f","makefile","clean"])
+
+	end = time.time()
+	print('This took me', end-start, 'seconds.')
